c1da98672572756f7a403971e3f726ed92898aab
Eugenio Salvador Arellano Ruiz <euarellano2192@gmail.com>
2024-04-13 20:57:02 +0200

    Advanced general struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,47 @@
 
 extern crate horned_owl;
+extern crate petgraph;
 
+use std::collections::HashMap;
+use horned_owl::model::*;
+use horned_owl::visitor::Visit;
+use horned_owl::model::ForIRI;
+use horned_owl::model::IRI;
+use petgraph::adj::*;
+use petgraph::graph::Graph;
+use petgraph::adj::IndexType;
+use std::io::Write;
 
-pub mod entity {
-    use horned_owl::visitor::Visit;
-    use horned_owl::model::ForIRI;
-    use horned_owl::model::IRI;
-    use horned_owl::model::AnonymousIndividual;
-    use horned_owl::ontology::component_mapped::ComponentMappedOntology;
-
-    // pub struct RLOntologyFilter(ComponentMappedOntology);
+pub trait RenderOnt<A: ForIRI> {
+    fn render_ontology(&mut self) {}
 }
 
+pub struct TaxonomyGraph<A, K>{
+    nodes: Vec<A>,
+    edges: Vec<(A,A)>,
+    map: HashMap<K,A>
+    
+}
+// pub struct RLOntologyFilter(ComponentMappedOntology);
+impl<A,K> Default for TaxonomyGraph<A,K> {
+    fn default() -> Self {
+        TaxonomyGraph { nodes: vec![], edges: vec![], map: HashMap::new() }
+}
+}
+
+impl<A,K> TaxonomyGraph<A,K> where 
+    A: Into<NodeIndex> + Default + Copy{
+    pub fn into_graph(self) -> Graph<A, A> {
+        Graph::<A, A>::from_edges(&self.edges)
+    }
+}
 #[cfg(test)]
 mod test {
     use horned_owl::io::owx::reader::read_with_build;
     use horned_owl::model::Build;
     use horned_owl::ontology::set::SetOntology;
-    use horned_owl::visitor::*;
+    use horned_owl::visitor::Walk;
+    use super::*;
 
     use std::io::BufRead;
 
@@ -34,7 +58,7 @@ mod test {
         let ont_s = include_str!("../tmp/bfo.owx");
         let ont = read_ok(&mut ont_s.as_bytes());
 
-        let mut walk = Walk::new(entity::IRIExtract::default());
+        let mut walk = Walk::new(TaxonomyGraph::default());
         walk.set_ontology(&ont);
         let mut v = walk.into_visit().into_vec();
         v.sort();
